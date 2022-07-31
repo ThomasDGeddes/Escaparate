b19d4de4fb50d81dc707b9b222f2aaa4fe4d0ec6
TGeddes <tgeddes@uncc.edu>
2022-07-31 14:24:41 -0400

    Initializing Inmar API
    - Updated API to use business, service, and data access layers.

---
+++ InmarAPI/InmarAPI/Business/Forecast.cs
@@ -0,0 +1,24 @@
+﻿namespace InmarAPI.Business
+{
+    public class Forecast
+    {
+        private static readonly string[] summaries = new string[]
+        {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public static WeatherForecast[] CreateWeatherForcast()
+        {
+            WeatherForecast[] forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = summaries[Random.Shared.Next(summaries.Length)]
+                })
+                .ToArray();
+
+            return forecast;
+        }
+    }
+}
---
+++ InmarAPI/InmarAPI/Service/Weather.cs
@@ -0,0 +1,16 @@
+﻿using InmarAPI.Business;
+
+namespace InmarAPI.Service
+{
+    public class Weather
+    {
+        public void GetWeather(WebApplication app)
+        {
+            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            {
+                return Forecast.CreateWeatherForcast();
+            })
+            .WithName("GetWeatherForecast");
+        }
+    }
+}
